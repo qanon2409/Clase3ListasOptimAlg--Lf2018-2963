2a8f7ca32b294be95fa3c047629af1a2d9453384
RonelRC <rr2019-3341@uce.edu.do>
2022-08-19 15:44:35 -0400

    Cambio 2 Ronel -Se cambiaron los nombres de los metodos en la clase Empleados

--- Clase3ListasOptimAlg/Empleados.cs
+++ Clase3ListasOptimAlg/Empleados.cs
@@ -17,7 +17,7 @@ namespace Clase3ListasOptimAlg
         public double td;
         public double ts;
 
-        public double CalcularSfs(double a)
+        public double CalculoSfs(double a)
         {
            // 0.0287
             double b = 0.8596;
@@ -25,20 +25,20 @@ namespace Clase3ListasOptimAlg
             return c;
         }
 
-        public double CalcularAfp(double p)
+        public double CalculoAfp(double p)
         {
             double z = 0.0304;
             double y = p * z;
             return y;
         }
 
-        public double CalcularTd(double a, double b)
+        public double CalculoTd(double a, double b)
         {
             double c = b + a;
             return c;
         }
 
-        public double CalcularTs(double a, double b)
+        public double CalculoTs(double a, double b)
         {
             double c = a - b;
             return c;
